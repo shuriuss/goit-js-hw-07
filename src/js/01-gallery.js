e40d5d29553738cff819c1903f67a03736c81db4
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-08-03 17:25:22 +0300

    add task1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+
+// Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const cardEl = createImg(galleryItems);
+
+galleryEl.addEventListener("click", handler);
+galleryEl.insertAdjacentHTML("beforeend", cardEl);
+
+function createImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+     </a>
+    </div>
+        `;
+    })
+    .join("");
+}
+
+function handler(evt) {
+  evt.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${evt.target.dataset.source}" width="800" height="600" >
+    </div>
+`);
+  instance.show();
+
+  window.addEventListener("keydown", abc);
+  function abc(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
